590c3ba40350e988031f22b73712f10ed52a368c
iuritorres <iuri.t1000@gmail.com>
2022-09-19 22:40:52 -0300

    teste de functions com state hook

--- React JS/statehook-onclick/src/componentes/PaginaInicial/index.js
+++ React JS/statehook-onclick/src/componentes/PaginaInicial/index.js
@@ -3,46 +3,81 @@ import './estilo.css';
 
 export default function PaginaInicial() {
 
+  // Mudar pro tema Dark
+  const temaLight = 'tema Light';
+  const temaDark = 'tema Dark';
+
+  const [ tema, setTema ] = useState(temaLight);
+
+  function mudarTema() {
+    const novoTema = tema === temaLight ? temaDark : temaLight;
+    setTema(novoTema);
+  };
+
+
+  // Gerar número aleatório
   const [ numeroAleatorio, setNumeroAleatorio ] = useState(0);
 
   function gerarNumero() {
     const novoNumero  = Math.floor(Math.random() * (100-1) + 1);
     setNumeroAleatorio(novoNumero);
-  }
+  };
 
+
+  // Gerar texto aleatório
   const [ novoTexto, setNovoTexto ] = useState();
 
+  const arrayTextos = [ 'Hello World!', 'ihuaaaaaa', 'a gara gan', 'KKKKKKK', 'Reri we go aguein...' ];
+
   function alterarTexto() {
-    setNovoTexto('Hello World!');
-  }
+    const posicaoArray = Math.floor(Math.random() * (arrayTextos.length - 0) + 0);
+
+    const textoAleatorio = arrayTextos[posicaoArray];
+    setNovoTexto(textoAleatorio);
+  };
+
 
   return(
-    <div className="conteudo-centralizado">
-        <h3>Número aleatório:</h3>
-        <h1>{ numeroAleatorio }</h1>
-        
-        <div className='area-botao'>
-          <label>
-            Click no botão abaixo para gerar um número aleatório:
-          </label>
+    <div>
+
+      <div className="head-box conteudo-centralizado">
+        <h3>Estudo State Hook e onClick</h3>
+
+        <button className="theme-button" onClick={ mudarTema }>{ tema }</button>
+      </div>
+
+      <div className="conteudo-centralizado">
+          <h3>Número aleatório:</h3>
+          <h1>{ numeroAleatorio }</h1>
           
-          <button onClick={ gerarNumero } >
-            Gerar número
-          </button>
-
-          <div className="area-texto">
-            <h2>{ novoTexto }</h2>
+          <div className='area-botao'>
+            <label>
+              Click no botão abaixo para gerar um número aleatório:
+            </label>
+            
+            <button onClick={ gerarNumero } >
+              Gerar número
+            </button>
           </div>
-          <label>
-            Click no botão abaixo para aparecer um texto:
-          </label>
-
-          <button onClick={ alterarTexto }>
-            Gerar texto
-          </button>
+          
         </div>
 
+      <div className="conteudo-centralizado">
+
+        <div className="area-texto">
+          <h2>{ novoTexto }</h2>
+        </div>
+
+        <label>
+          Click no botão abaixo para aparecer um texto:
+        </label>
+
+        <button onClick={ alterarTexto }>
+          Gerar texto
+        </button>
       </div>
+
+    </div>
   );
 }
 
