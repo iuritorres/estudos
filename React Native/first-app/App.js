f87eb030ae9c18e5875c2397ab269cd7ab840078
Iuri Torres <iuri.t1000@gmail.com>
2022-12-17 14:55:06 -0300

    changed app routes on react native app

--- React Native/first-app/App.js
+++ React Native/first-app/App.js
@@ -9,33 +9,38 @@ import TelaConsoles from "./Screens/TelaConsoles";
 import TelaJogos from "./Screens/TelaJogos";
 import TelaNuvem from "./Screens/TelaNuvem";
 
-// const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 
-// // App
-// export default function App() {
-//   return (
-//       <NavigationContainer>
-//         <Drawer.Navigator>
-
-//           <Drawer.Screen name="MainScreen" component={ MainScreen } />
-//           <Drawer.Screen name="About" component={ About } />
-
-//         </Drawer.Navigator>
-//       </NavigationContainer>
-//   );
-// }
-
-const Stack = createStackNavigator();
-
+// App
 export default function App() {
   return (
-    <NavigationContainer >
-      <Stack.Navigator>
-        <Stack.Screen name="Tela inicial" component={TelaInicial} />
-        <Stack.Screen name="Consoles" component={TelaConsoles} />
-        <Stack.Screen name="Jogos" component={TelaJogos} />
-        <Stack.Screen name="Nuvem" component={TelaNuvem} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-};
+      <NavigationContainer>
+        <Drawer.Navigator>
+
+          <Drawer.Screen name="MainScreen" component={ MainScreen } />
+          <Drawer.Screen name="About" component={ About } />
+
+          <Drawer.Screen name="Tela inicial" component={TelaInicial} />
+          <Drawer.Screen name="Consoles" component={TelaConsoles} />
+          <Drawer.Screen name="Jogos" component={TelaJogos} />
+          <Drawer.Screen name="Nuvem" component={TelaNuvem} />
+
+        </Drawer.Navigator>
+      </NavigationContainer>
+  );
+}
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer >
+//       <Stack.Navigator>
+//         <Stack.Screen name="Tela inicial" component={TelaInicial} />
+//         <Stack.Screen name="Consoles" component={TelaConsoles} />
+//         <Stack.Screen name="Jogos" component={TelaJogos} />
+//         <Stack.Screen name="Nuvem" component={TelaNuvem} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   )
+// };
